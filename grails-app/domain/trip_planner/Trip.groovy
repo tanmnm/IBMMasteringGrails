8ccb59780ef5957d167b4126032d404bd7efad6c
unknown <wrien6297@yahoo.com>
2014-05-26 15:20:34 -0400

    Created project

---
+++ grails-app/domain/trip_planner/Trip.groovy
@@ -0,0 +1,13 @@
+package trip_planner
+
+class Trip {
+    String name
+    String city
+    Date startDate
+    Date endDate
+    String purpose
+    String notes
+    
+    static constraints = {
+    }
+}
